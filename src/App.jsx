561d80f995e0427a42ef469c646e802b46b0d274
goodnewsesoh-lang <goodnewsesoh@gmail.com>
2026-09-17 03:58:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@ import Genres from './pages/Genres.jsx';
 import GenreDetail from './pages/GenreDetail.jsx';
 import Search from './pages/Search.jsx';
 import TitleDetails from './pages/TitleDetails.jsx';
+import CollectionDetail from './pages/CollectionDetail.jsx';
 
 import AdminLogin from './pages/admin/Login.jsx';
 import AdminDashboard from './pages/admin/Dashboard.jsx';
 import ManageTitles from './pages/admin/ManageTitles.jsx';
 import TitleForm from './pages/admin/TitleForm.jsx';
+import Collections from './pages/admin/Collections.jsx';
+import CollectionForm from './pages/admin/CollectionForm.jsx';
 
 export default function App() {
   return (
@@ -28,12 +31,16 @@ export default function App() {
           <Route path="/genres/:name" element={<GenreDetail />} />
           <Route path="/search" element={<Search />} />
           <Route path="/title/:id" element={<TitleDetails />} />
+          <Route path="/collections/:slug" element={<CollectionDetail />} />
 
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
           <Route path="/admin/titles" element={<ProtectedRoute><ManageTitles /></ProtectedRoute>} />
           <Route path="/admin/titles/new" element={<ProtectedRoute><TitleForm /></ProtectedRoute>} />
           <Route path="/admin/titles/:id/edit" element={<ProtectedRoute><TitleForm /></ProtectedRoute>} />
+          <Route path="/admin/collections" element={<ProtectedRoute><Collections /></ProtectedRoute>} />
+          <Route path="/admin/collections/new" element={<ProtectedRoute><CollectionForm /></ProtectedRoute>} />
+          <Route path="/admin/collections/:id/edit" element={<ProtectedRoute><CollectionForm /></ProtectedRoute>} />
         </Routes>
       </main>
       <Footer />
